fb7b8c90c4d743b8d93122ce8ca1f6bc41d36c26
Izukach <86355993+Izukach@users.noreply.github.com>
2022-07-25 20:42:58 -0700

    my responsive project

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function getEmail(){
+    var email = document.getElementById('email').value;
+    alert(email);
+}
+
+  function changeParagraphOne(){
+    // This is a block of code 
+    // document.getElementById("paragraph_one").innerHTML = 'Lorem Ipsum';
+    var izuka = 100;
+    var tgblinks = 300 ;
+    var total = izuka + tgblinks;
+    document.getElementById('paragraph_one').innerHTML = total;
+    // console.log(izuka);
+    // console.log(tgblinks);
+    // console.log(total);
+    // window.alert(total)
+  }
